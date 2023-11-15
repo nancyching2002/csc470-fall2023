44166f9ad7c22d38f7bc3b5130cbc96a062aa794
Nancy Ching <106937449+nancyching2002@users.noreply.github.com>
2023-11-15 17:44:44 -0500

    05 assignment

---
+++ assignments/05/Assets/GameManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager SharedInstance;
+    public List<UnitScript> units = new List<UnitScript>();
+    public GameObject unitPrefab;
+
+    UnitScript selectedUnit;
+
+    
+    void Awake()
+    {
+        if (SharedInstance != null)
+        {
+            Debug.Log("Why is there more than one GameManager!?!?!?!");
+        }
+        SharedInstance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Click");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 999999))
+            {
+                if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                {
+                    // If we get in here, we hit something! And the 'hit' object
+                    // contains info about what we hit.
+                    if (selectedUnit != null)
+                    {
+                        Debug.Log("Set target");
+                        selectedUnit.SetTarget(hit.point);
+                    }
+                }
+            }
+        }
+    }
+
+    public void SelectUnit(UnitScript unit)
+    {
+        // Deselect any units that think they are selected
+        foreach (UnitScript u in units) {
+            u.selected = false;
+            u.SetUnitColor();
+        }
+        selectedUnit = unit;
+        selectedUnit.selected = true;
+        selectedUnit.SetUnitColor();
+    }
+}
---
+++ assignments/05/Assets/UnitScript.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitScript : MonoBehaviour
+{
+    public Renderer bodyRenderer;
+    public CharacterController cc;
+
+    public Color selectedColor;
+    public Color hoverColor;
+    Color defaultColor;
+
+    float moveSpeed = 5;
+
+    bool hover = false;
+    public bool selected = false;
+
+    Vector3 target;
+    bool hasTarget = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultColor = bodyRenderer.material.color;
+        GameManager.SharedInstance.units.Add(this);
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.SharedInstance.units.Remove(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasTarget)
+        {
+            Vector3 vectorToTarget = (target - transform.position).normalized;
+            cc.Move(vectorToTarget * moveSpeed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, target) < 0.5f)
+            {
+                hasTarget = false;
+            }
+        }
+    }
+
+    public void SetTarget(Vector3 t)
+    {
+        target = t;
+        hasTarget = true;
+    }
+
+    private void OnMouseDown()
+    {
+        GameManager.SharedInstance.SelectUnit(this);
+        SetUnitColor(); 
+    }
+
+    private void OnMouseEnter()
+    {
+        hover = true;
+        SetUnitColor();
+        Debug.Log("Hovering");
+    }
+
+    private void OnMouseExit()
+    {
+        hover = false;
+        SetUnitColor();
+    }
+
+    public void SetUnitColor()
+    {
+        if (selected)
+        {
+            bodyRenderer.material.color = selectedColor;
+        }
+        else if (hover)
+        {
+            bodyRenderer.material.color = hoverColor;
+        }
+        else
+        {
+            bodyRenderer.material.color = defaultColor;
+        }
+    }
+}
